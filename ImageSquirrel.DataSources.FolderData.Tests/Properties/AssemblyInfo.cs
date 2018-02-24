f122910a7b8fb03a4fcebbf1851fa5936fe9d6d2
ltnublet <lord_onager@yahoo.com>
2018-02-23 22:49:15 -0500

    Update assembly infos with more useful values

--- ImageSquirrel.DataSources.FolderData.Tests/Properties/AssemblyInfo.cs
+++ ImageSquirrel.DataSources.FolderData.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,13 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ImageSquirrel.DataSources.FolderData.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImageSquirrel.DataSources.FolderData.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("ImageSquirrel FolderData DataSource Tests")]
+[assembly: AssemblyDescription("Tests for the ImageSquirrel FolderData DataSource plugin.")]
+[assembly: AssemblyProduct("ImageSquirrel")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("1501275c-df47-4f9d-bf2a-f407a4871257")]
+[assembly: AssemblyVersion("0.1.*")]
 
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("1501275c-df47-4f9d-bf2a-f407a4871257")]
--- ImageSquirrel.DataSources.ReferenceImplementation/Properties/AssemblyInfo.cs
+++ ImageSquirrel.DataSources.ReferenceImplementation/Properties/AssemblyInfo.cs
@@ -1,36 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ImageSquirrel.DataSources.ReferenceImplementation")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImageSquirrel.DataSources.ReferenceImplementation")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("ImageSquirrel DataSource Reference Implementations")]
+[assembly: AssemblyDescription("Reference implementations of DataSource interfaces.")]
+[assembly: AssemblyProduct("ImageSquirrel")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e2370be0-e35b-4123-8ab5-b78eb5a641c0")]
+[assembly: AssemblyVersion("0.1.*")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("e2370be0-e35b-4123-8ab5-b78eb5a641c0")]
